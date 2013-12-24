948bca3f03fa141bf24c7816111139ab500fdb6e
Patrick Linskey <plinskey@gmail.com>
2013-12-23 20:18:42 -0800

    copy the user-provided request data into the context's request

--- aetestutil/aetestutil.go
+++ aetestutil/aetestutil.go
@@ -16,7 +16,33 @@ func CreateAndRegisterTestContext() (context aetest.Context, err error) {
 
 	// TODO change this function to maintain and consult a map, so we can
 	// make multiple requests in parallel / in a recursive fashion
-	aeutil.ContextProvider = func(*http.Request) appengine.Context { return context }
+	aeutil.ContextProvider = func(req *http.Request) appengine.Context {
+		// Copy the request details from the user-created one to the
+		// context's one, and edit it to add in the Appengine headers,
+		// as seen in appengine/user/user_dev.go. This has to happen
+		// before we make any calls to the user module, which works
+		// out since this is called during UncurryWithContext.
+		contextRequest := context.Request().(*http.Request)
+		contextRequest.Body = req.Body
+		contextRequest.ContentLength = req.ContentLength
+		contextRequest.Form = req.Form
+		contextRequest.Header = req.Header
+		contextRequest.Host = req.Host
+		contextRequest.Method = req.Method
+		contextRequest.MultipartForm = req.MultipartForm
+		contextRequest.PostForm = req.PostForm
+		contextRequest.Proto = req.Proto
+		contextRequest.ProtoMajor = req.ProtoMajor
+		contextRequest.ProtoMinor = req.ProtoMinor
+		contextRequest.RemoteAddr = req.RemoteAddr
+		contextRequest.RequestURI = req.RequestURI
+		contextRequest.TLS = req.TLS
+		contextRequest.Trailer = req.Trailer
+		contextRequest.TransferEncoding = req.TransferEncoding
+		contextRequest.URL = req.URL
+
+		return context
+	}
 
 	return
 }
