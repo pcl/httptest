146b98633c2bcf120046ee73d8000c18c09b4d0f
Patrick Linskey <plinskey@cisco.com>
2020-09-09 21:06:46 +0200

    upgrade to more-modern go

--- aeutil/aeutil.go
+++ aeutil/aeutil.go
@@ -1,22 +1,23 @@
 package aeutil
 
 import (
+	"context"
 	"net/http"
-	"appengine"
+	"google.golang.org/appengine"
 )
 
 var (
 	// To be set by an external environment, if it wants control over context creation
-	ContextProvider func(*http.Request) appengine.Context
+	ContextProvider func(*http.Request) context.Context
 )
 
 func init() {
-	ContextProvider = func(r *http.Request) appengine.Context {
+	ContextProvider = func(r *http.Request) context.Context {
 		return appengine.NewContext(r)
 	}
 }
 
-func UncurryWithContext(handler func(http.ResponseWriter, *http.Request, appengine.Context)) func(http.ResponseWriter, *http.Request) {
+func UncurryWithContext(handler func(http.ResponseWriter, *http.Request, context.Context)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		handler(w, r, ContextProvider(r))
 	}
